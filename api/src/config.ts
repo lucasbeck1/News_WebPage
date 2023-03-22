04ca4213cc605cbff87b40b73aa2e7f325cf5e49
lucasbeck1 <lucasbeckford1@gmail.com>
2023-03-22 14:56:42 -0300

    refactor(back): set credentials in .env file

--- api/src/config.ts
+++ api/src/config.ts
@@ -3,3 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 3001;
+export const db_type = process.env.db_type || "mysql";
+export const db_host = process.env.db_host || "localhost";
+export const db_name = process.env.db_name || "db_name";
+export const db_port: number = Number(process.env.db_port) || 3306;
+export const db_username = process.env.db_username || "db_username";
+export const db_password = process.env.db_password || "db_password";
--- api/src/db.ts
+++ api/src/db.ts
@@ -4,16 +4,17 @@ import { Author } from "./entities/authorEntity";
 import { Section } from "./entities/sectionEntity";
 import { Sponsor } from "./entities/sponsorEntity";
 import { Publicity } from "./entities/publicityEntity";
+import { db_host, db_name, db_port, db_username, db_password } from "./config";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "root123456",
-  database: "rocky_db",
+  host: db_host,
+  port: db_port,
+  username: db_username,
+  password: db_password,
+  database: db_name,
   synchronize: true,
   entities: [Article, Author, Section, Sponsor, Publicity],
+  //entities: ["entities/*.ts"],
   // logging: true,
-  // entities: ["entities/*.ts"],
 });
