95227ad0ea457af121c7a198384c663d64204e57
lucasbeck1 <lucasbeckford1@gmail.com>
2023-03-30 20:02:09 -0300

    feat(front): change detail structure

--- client/src/pages/Detail.tsx
+++ client/src/pages/Detail.tsx
@@ -1,10 +1,10 @@
 import Container from "@mui/material/Container";
 import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
 import Header from "../components/header";
-import CardArticle from "../components/card";
 import Footer from "../components/footer";
 import articles from "../dataExamples/articles.json";
 import { useParams } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 function Detail() {
   const { id } = useParams();
@@ -12,20 +12,33 @@ function Detail() {
     (article) => article.id.toString() === id
   );
 
+  const articleModify: boolean =
+    selectedArricle?.updatedAt !== selectedArricle?.createdAt ? true : false;
+
   return (
     <>
       <Container maxWidth="lg" sx={{ p: 0 }}>
         <ScopedCssBaseline enableColorScheme>
           <Header />
-          <h1>{selectedArricle?.headline}</h1>
-          <img src={selectedArricle?.image} alt="article loading" />
-          <h4>{selectedArricle?.drophead}</h4>
-          <p>{selectedArricle?.createdAt}</p>
-          <p>By {selectedArricle?.author}</p>
-          <p>{selectedArricle?.body}</p>
-          <p>
-            This article was updated lastime on {selectedArricle?.updatedAt}
-          </p>
+
+          <Box sx={{ flexGrow: 1, pt: 2, pb: 2, pl: 4, pr: 4 }}>
+            <h1>{selectedArricle?.headline}</h1>
+            <h4>{selectedArricle?.drophead}</h4>
+            <img
+              src={selectedArricle?.image}
+              alt="article loading"
+              width={"100%"}
+              style={{ objectFit: "contain", maxHeight: "20rem" }}
+            />
+            <p>{selectedArricle?.body}</p>
+            <p style={{ display: "flex", justifyContent: "flex-start" }}>
+              {articleModify
+                ? `(Updated) ${selectedArricle?.updatedAt}`
+                : selectedArricle?.createdAt}{" "}
+              By {selectedArricle?.author}
+            </p>
+          </Box>
+
           <Footer />
         </ScopedCssBaseline>
       </Container>
