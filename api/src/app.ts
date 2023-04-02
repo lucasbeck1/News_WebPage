6e9f84964ca2719da30f90aadf56d877dbd326dd
lucasbeck1 <lucasbeckford1@gmail.com>
2023-04-02 16:29:07 -0300

    chore(back): remove urlencoded middleware

--- api/src/app.ts
+++ api/src/app.ts
@@ -15,7 +15,6 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Routers
 app.use("/status", statusRouter);
