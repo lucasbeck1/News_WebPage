55f4770488a97a78c44ac362c644a9bf4bdd6228
lucasbeck1 <lucasbeckford1@gmail.com>
2023-04-27 00:44:25 -0300

    feat(back): add unique name to images

--- api/src/controllers/imageController.ts
+++ api/src/controllers/imageController.ts
@@ -7,7 +7,9 @@ import path from "path";
 const storage = multer.diskStorage({
   destination: path.join(__dirname, "../../uploads"),
   filename: (_req, file, cb) => {
-    cb(null, file.originalname);
+    const date = new Date();
+    const fileName = date.toISOString() + "_" + file.originalname;
+    cb(null, fileName);
   },
 });
 
@@ -23,6 +25,8 @@ export const uploadImage = async (req: Request, res: Response) => {
       return res.status(500).json({ message: err.message });
     }
     console.log(req.file);
-    return res.status(200).json({ message: "Upload Ok" });
+    return res
+      .status(200)
+      .json({ message: "Upload Ok", fileName: req.file?.filename });
   });
 };
