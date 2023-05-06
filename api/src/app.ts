bfb8520e625d71e02b26b1a0c6da4731d293c651
lucasbeck1 <lucasbeckford1@gmail.com>
2023-05-06 02:43:55 -0300

    feat(back): add cors config

--- api/src/app.ts
+++ api/src/app.ts
@@ -17,12 +17,26 @@ import authRoutes from "./routes/authRoutes";
 
 // Express and midlewares
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser(cookieKey));
 const staticPath = path.join(__dirname, "uploads");
 app.use(express.static(staticPath));
+/* 
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+}); 
+*/
 
 // Routers
 app.use("/status", statusRouter);
