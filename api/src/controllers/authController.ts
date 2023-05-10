eb6dd7c4e12e7acaad6ef568c3467317f229cb7f
lucasbeck1 <lucasbeckford1@gmail.com>
2023-05-10 01:43:46 -0300

    feat(back): add auth verify functions

--- api/src/controllers/authController.ts
+++ api/src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { Author } from "../entities/authorEntity";
 import { saltRounds } from "../config";
 import bcrypt from "bcrypt";
@@ -13,6 +13,8 @@ type registerRequest = {
   mail: string;
   password: string;
 };
+
+// ------------------------------------------------------------------
 
 export const login = async (
   req: Request<unknown, unknown, loginRequest>,
@@ -43,6 +45,8 @@ export const login = async (
   }
 };
 
+// ------------------------------------------------------------------
+
 export const logOut = async (
   req: Request<unknown, unknown, loginRequest>,
   res: Response
@@ -62,6 +66,8 @@ export const logOut = async (
     return res.send(error);
   }
 };
+
+// ------------------------------------------------------------------
 
 export const register = async (
   req: Request<unknown, unknown, registerRequest>,
@@ -106,4 +112,60 @@ export const register = async (
       return res.status(500).json({ message: "Response Error" });
     }
   }
+};
+
+// ------------------------------------------------------------------
+
+export const adminVerification = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.signedCookies;
+  const { name, admin } = req.cookies;
+
+  if (!id || !name || !admin || admin !== "true") {
+    return res.status(404).json({ message: "Invalid request" });
+  }
+
+  const adminAuthor = await Author.findOne({
+    where: { id: id },
+    select: { name: true, admin: true },
+  });
+
+  if (!adminAuthor || adminAuthor.name !== name || adminAuthor.admin !== true) {
+    return res.status(400).json({ message: "Invalid request" });
+  }
+
+  return next();
+};
+
+// ------------------------------------------------------------------
+
+export const authorVerification = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.signedCookies;
+  const { name, admin } = req.cookies;
+
+  if (!id || !name || !admin || admin !== "false") {
+    return res.status(404).json({ message: "Invalid request" });
+  }
+
+  const adminAuthor = await Author.findOne({
+    where: { id: id },
+    select: { name: true, admin: true },
+  });
+
+  if (
+    !adminAuthor ||
+    adminAuthor.name !== name ||
+    adminAuthor.admin !== false
+  ) {
+    return res.status(400).json({ message: "Invalid request" });
+  }
+
+  return next();
 };
