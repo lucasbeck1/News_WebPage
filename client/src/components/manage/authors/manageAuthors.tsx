5c12c8866a8834348765352c1660d778399d1600
lucasbeck1 <lucasbeckford1@gmail.com>
2023-05-11 15:35:06 -0300

    feat(front): add loading on manage authors

--- client/src/components/manage/authors/manageAuthors.tsx
+++ client/src/components/manage/authors/manageAuthors.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ContentLoader from "react-content-loader";
 import { RootState } from "../../../store";
 
 import { getAuthors } from "../../../services/authors/actions";
@@ -19,6 +20,7 @@ import TablePagination from "@mui/material/TablePagination";
 import { Box, Button, IconButton } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import CheckIcon from "@mui/icons-material/Check";
+import { all } from "axios";
 
 type Author = {
   id: string;
@@ -75,56 +77,129 @@ function ManageAuthors() {
     <>
       <Paper sx={{ width: "100%" }}>
         <TableContainer component={Paper} sx={{ minHeight: 410 }}>
-          <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>Name</StyledTableCell>
-                <StyledTableCell align="right">Mail</StyledTableCell>
-                <StyledTableCell align="right">Admin</StyledTableCell>
-                <StyledTableCell align="right">Actions</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {allAuthors
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((user) => (
-                  <StyledTableRow
-                    key={user.name}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <StyledTableCell component="th" scope="row">
-                      {user.name}
-                    </StyledTableCell>
-                    <StyledTableCell align="right">{user.mail}</StyledTableCell>
-                    <StyledTableCell align="right">
-                      {user.admin ? (
-                        <IconButton
-                          sx={{ p: 0, m: 0 }}
-                          size="small"
-                          aria-label="modify"
-                          color="primary"
-                        >
-                          <CheckIcon />
-                        </IconButton>
-                      ) : (
-                        <IconButton
-                          sx={{ p: 0, m: 0 }}
-                          size="small"
-                          aria-label="modify"
-                          color="default"
-                        >
-                          <ClearIcon />
-                        </IconButton>
-                      )}
-                    </StyledTableCell>
-                    <StyledTableCell align="right">
-                      <ManageModify user={user} />
-                      <ManageDelete author={user} />
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-            </TableBody>
-          </Table>
+          {allAuthors ? (
+            <Table
+              sx={{ minWidth: 650 }}
+              size="small"
+              aria-label="a dense table"
+            >
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Name</StyledTableCell>
+                  <StyledTableCell align="right">Mail</StyledTableCell>
+                  <StyledTableCell align="right">Admin</StyledTableCell>
+                  <StyledTableCell align="right">Actions</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {allAuthors
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((user) => (
+                    <StyledTableRow
+                      key={user.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <StyledTableCell component="th" scope="row">
+                        {user.name}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {user.mail}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {user.admin ? (
+                          <IconButton
+                            sx={{ p: 0, m: 0 }}
+                            size="small"
+                            aria-label="modify"
+                            color="primary"
+                          >
+                            <CheckIcon />
+                          </IconButton>
+                        ) : (
+                          <IconButton
+                            sx={{ p: 0, m: 0 }}
+                            size="small"
+                            aria-label="modify"
+                            color="default"
+                          >
+                            <ClearIcon />
+                          </IconButton>
+                        )}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        <ManageModify user={user} />
+                        <ManageDelete author={user} />
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <>
+              <Table
+                sx={{ minWidth: 650 }}
+                size="small"
+                aria-label="a dense table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Name</StyledTableCell>
+                    <StyledTableCell align="right">Mail</StyledTableCell>
+                    <StyledTableCell align="right">Admin</StyledTableCell>
+                    <StyledTableCell align="right">Actions</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+
+              <ContentLoader
+                speed={2.5}
+                backgroundColor="#d4d4d4"
+                foregroundColor="#898989"
+                viewBox="0 0 180 70"
+              >
+                <rect
+                  x="5"
+                  y="5"
+                  rx="1"
+                  ry="1"
+                  width="10.5rem"
+                  height="0.4rem"
+                />
+                <rect
+                  x="5"
+                  y="15"
+                  rx="1"
+                  ry="1"
+                  width="10.5rem"
+                  height="0.4rem"
+                />
+                <rect
+                  x="5"
+                  y="25"
+                  rx="1"
+                  ry="1"
+                  width="10.5rem"
+                  height="0.4rem"
+                />
+                <rect
+                  x="5"
+                  y="35"
+                  rx="1"
+                  ry="1"
+                  width="10.5rem"
+                  height="0.4rem"
+                />
+                <rect
+                  x="5"
+                  y="45"
+                  rx="1"
+                  ry="1"
+                  width="10.5rem"
+                  height="0.4rem"
+                />
+              </ContentLoader>
+            </>
+          )}
         </TableContainer>
         <Box
           sx={{
@@ -141,7 +216,7 @@ function ManageAuthors() {
           <TablePagination
             rowsPerPageOptions={[10, 25, 100]}
             component="div"
-            count={allAuthors.length}
+            count={allAuthors.length || 0}
             rowsPerPage={rowsPerPage}
             page={page}
             onPageChange={handleChangePage}
