3081c748ab8f45ce0caad609ac35ca5b3ba1d2ae
lucasbeck1 <lucasbeckford1@gmail.com>
2023-06-06 14:23:05 -0300

    feat(back): add  publicity routes

--- api/src/routes/sponsor/publicitySponsor.ts
+++ api/src/routes/sponsor/publicitySponsor.ts
@@ -2,11 +2,18 @@ import { Router } from "express";
 import {
   getPublicityBySponsor,
   createPublicity,
+  updatePublicity,
+  deletePublicity,
 } from "../../controllers/publicityController";
 
 const router = Router();
 
 router.get("/:name", getPublicityBySponsor);
+
 router.post("/", createPublicity);
+
+router.put("/:id", updatePublicity);
+
+router.delete("/:id", deletePublicity);
 
 export default router;
